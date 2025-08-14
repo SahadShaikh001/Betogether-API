1a4e4ce6e081b3f52ee26c8221da65463dc3050d
SahadShaikh001 <sahadshaikh877@gmail.com>
2025-08-14 20:07:40 +0530

    Update otp_utils.py

--- utils/otp_utils.py
+++ utils/otp_utils.py
@@ -2,7 +2,8 @@ import random
 from datetime import datetime, timedelta
 
 def generate_otp():
-    """Returns a 4-digit OTP and expiry time (5 min)."""
+    """Returns a 4-digit OTP and expiry time (2 min)."""
     otp_code = str(random.randint(1000, 9999))  # 4-digit
-    otp_expiry = datetime.utcnow() + timedelta(minutes=5)
+    otp_expiry = datetime.utcnow() + timedelta(minutes=2)
     return otp_code, otp_expiry
+
