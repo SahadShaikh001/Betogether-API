9b5bf5e45e2be1e7b7082a54b759aa68c49b6301
SahadShaikh001 <sahadshaikh877@gmail.com>
2025-08-14 20:07:11 +0530

    Update email_utils.py

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -8,7 +8,7 @@ SMTP_PASSWORD = "uxzj sdjg snrb zoor"
 
 def send_otp_email(to_email: str, otp_code: str):
     subject = "Your OTP Code"
-    body = f"Your OTP code is {otp_code}. It will expire in 10 minutes."
+    body = f"Your OTP code is {otp_code}. It will expire in 2 minutes."
     msg = MIMEText(body)
     msg["Subject"] = subject
     msg["From"] = SMTP_EMAIL
@@ -18,3 +18,4 @@ def send_otp_email(to_email: str, otp_code: str):
         server.starttls()
         server.login(SMTP_EMAIL, SMTP_PASSWORD)
         server.sendmail(SMTP_EMAIL, to_email, msg.as_string())
+
