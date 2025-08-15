954d527f0332c82361d8c293b8b078ef0dca5a22
SahadShaikh001 <sahadshaikh877@gmail.com>
2025-08-15 16:00:54 +0530

    Update email_utils.py

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -1,5 +1,7 @@
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import datetime
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
@@ -7,15 +9,24 @@ SMTP_EMAIL = "sahadshaikh2209@gmail.com"
 SMTP_PASSWORD = "uxzj sdjg snrb zoor"
 
 def send_otp_email(to_email: str, otp_code: str):
-    subject = "Your OTP Code"
-    body = f"Your OTP code is {otp_code}. It will expire in 2 minutes."
-    msg = MIMEText(body)
-    msg["Subject"] = subject
+    # Read and fill HTML template
+    with open("templates/email_otp.html", "r") as f:
+        html_template = f.read()
+
+    html_content = html_template.replace("{{otp_code}}", otp_code)
+    html_content = html_content.replace("{{date}}", datetime.now().strftime("%d %B %Y"))
+
+    # Create email
+    msg = MIMEMultipart("alternative")
+    msg["Subject"] = "Your OTP Code"
     msg["From"] = SMTP_EMAIL
     msg["To"] = to_email
 
+    # Attach HTML
+    msg.attach(MIMEText(html_content, "html"))
+
+    # Send email
     with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
         server.starttls()
         server.login(SMTP_EMAIL, SMTP_PASSWORD)
         server.sendmail(SMTP_EMAIL, to_email, msg.as_string())
-
